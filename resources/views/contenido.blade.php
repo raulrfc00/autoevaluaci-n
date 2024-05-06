340d41967b1ac7284e2d1ad3b8cce66b7c4ad9be
raulrfc00 <rruizdelafuentec2223@politecnics.barcelona>
2024-05-06 18:44:26 +0200

    a

--- resources/views/contenido.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('plantilla.autoevaluacion')
-
-@section('content')
-
-    <img src="http://localhost/M07/laravel/autoevaluacion_raul_ruiz_de_la_fuente/public/logo.png" alt="logo" style="height: 200px;">
-@endsection
